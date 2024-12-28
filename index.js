d97e6a6e798829cf801e73ec6dc0760843b3f50a
Gokul Sami <118269253+Gokul-sami@users.noreply.github.com>
2024-12-28 20:40:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.post('/create', (req, res) => {
+    res.render('createPost.ejs');
+});
+
+var title = [];
+var content = [];
+app.post('/submit', (req, res) => {
+    if(req.body.mode == 'Delete' && req.body.index != null){
+        title.splice(req.body.index, 1);
+        content.splice(req.body.index, 1);
+    }
+    else if(req.body.mode == 'Submit' && req.body.index != null){
+        title[req.body.index] = req.body.title;
+        content[req.body.index] = req.body.content;
+    }
+    else{
+        title.push(req.body.title);
+        content.push(req.body.content);
+    }
+    res.render('index.ejs', { title: title, content: content });
+});
+
+app.post('/edit', (req, res) => {
+    res.render('editPost.ejs', { title: title, content: content, index: req.body.index });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
