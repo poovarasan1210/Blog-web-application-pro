8f1ba968fb25799f779ab4cec130b51facd9bbce
Gokul-sami <gokulpandiyan6@gmail.com>
2025-01-16 13:26:31 +0530

    import client properly

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { Client } from 'pg';
+import pkg from 'pg';
+const { Client } = pkg;
 
 const app = express();
 const PORT = process.env.PORT || 3000;
