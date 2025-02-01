153e9783384c9e7dfd05e2fc65a122a1ab3e584b
Gokul-sami <gokulpandiyan6@gmail.com>
2025-02-01 12:06:12 +0530

    login and registe add

--- server.js
+++ server.js
@@ -2,52 +2,82 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import env from 'dotenv';
 import pkg from 'pg';
+import bcrypt from "bcrypt";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import session from "express-session";
 const { Client } = pkg;
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const saltRounds = 10;
 env.config();
+
+app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: true,
+    })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const db = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-        rejectUnauthorized: false,
+        rejectUnauthorized: true,
     }
 });
+// const db = new Client({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+//     ssl: false  // Explicitly disable SSL
+// });
 db.connect();
-
-const createTableQuery = `
-    CREATE TABLE IF NOT EXISTS blogs (
-        id SERIAL PRIMARY KEY,
-        title VARCHAR(255) NOT NULL,
-        content TEXT NOT NULL,
-        author VARCHAR(255) NOT NULL,
-        date VARCHAR(50) NOT NULL
-    )
-`;
-
-db.query(createTableQuery)
-    .then(() => console.log("Table 'blogs' is ready"))
-    .catch(err => console.error("Error creating table", err));
 
 var blogs = [];
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+    res.redirect("/home");
+});
 
-app.use(express.static('public'));
-
-app.get('/', async (req, res) => {
+app.get('/home', async (req, res) => {
     try{
-        const result = await db.query("select * from blogs order by id asc");
-        blogs = result.rows;
+        if (req.isAuthenticated()) {
+            const result = await db.query("select * from blogs order by id asc");
+            blogs = result.rows;
 
-        res.render('index.ejs', {
-            bgs: blogs
-        });
+            res.render('index.ejs', {
+                bgs: blogs
+            });
+        } else {
+            res.redirect("/login");
+        }
     }
     catch(err){
         console.log(err);
     }
+});
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+});
+
+app.get("/logout", (req, res) => {
+    req.logout(function (err) {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
 });
 
 app.post('/create', async (req, res) => {
@@ -91,6 +121,92 @@ app.post('/submit', async (req, res) => {
 
 app.post('/edit', (req, res) => {
     res.render('editPost.ejs', { title: req.body.title, content: req.body.content, author: req.body.author, id: req.body.id });
+});
+
+app.post(
+    "/login",
+    passport.authenticate("local", {
+      successRedirect: "/home",
+      failureRedirect: "/register",
+      failureFlash: true,
+    })
+);
+
+app.get("/register", (req, res) => {
+    res.render("register.ejs");
+});
+  
+app.post("/register", async (req, res) => {
+    const email = req.body.username;
+    const password = req.body.password;
+  
+    try {
+      const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
+        email,
+      ]);
+  
+      if (checkResult.rows.length > 0) {
+        req.redirect("/login");
+      } else {
+        bcrypt.hash(password, saltRounds, async (err, hash) => {
+          if (err) {
+            console.error("Error hashing password:", err);
+          } else {
+            const result = await db.query(
+              "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
+              [email, hash]
+            );
+            const user = result.rows[0];
+            req.login(user, (err) => {
+              console.log("success");
+              res.redirect("/home");
+            });
+          }
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+});
+  
+passport.use(
+    new Strategy(async function verify(username, password, cb) {
+      try {
+        const result = await db.query("SELECT * FROM users WHERE email = $1 ", [
+          username,
+        ]);
+        if (result.rows.length > 0) {
+          const user = result.rows[0];
+          const storedHashedPassword = user.password;
+          bcrypt.compare(password, storedHashedPassword, (err, valid) => {
+            if (err) {
+              //Error with password check
+              console.error("Error comparing passwords:", err);
+              return cb(err);
+            } else {
+              if (valid) {
+                //Passed password check
+                return cb(null, user);
+              } else {
+                //Did not pass password check
+                return cb(null, false);
+              }
+            }
+          });
+        } else {
+          return cb("User not found");
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    })
+);
+  
+passport.serializeUser((user, cb) => {
+    cb(null, user);
+});
+passport.deserializeUser((user, cb) => {
+    cb(null, user);
 });
 
 app.listen(PORT, () => {
